0e2aa9650ca8a59478d6e17d72b9a322efa5bfce
thejitenpatel <jiten.pateljp2001@gmail.com>
2024-01-30 19:48:29 +0530

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Jiten Patel on 30/01/24.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
